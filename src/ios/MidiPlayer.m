d8945913706a1e023ee6d58f72b55bc34c71cd06
Sorawit Suriyakarn <thepsint@gmail.com>
2016-03-02 00:45:52 -0500

    update ios gettime

--- src/ios/MidiPlayer.m
+++ src/ios/MidiPlayer.m
@@ -14,7 +14,7 @@ BOOL paused = NO;
     NSString* path = [command.arguments objectAtIndex:0];
     NSArray* programs = [command.arguments objectAtIndex:1];
     setupCallbackId = command.callbackId;
-    
+
     [self.commandDelegate runInBackground:^{
         //NSLog(@"Setup background");
         CDVPluginResult* pluginResult = nil;
@@ -27,7 +27,7 @@ BOOL paused = NO;
                 paused = NO;
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
                 [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
-                
+
                 [self.commandDelegate sendPluginResult:pluginResult callbackId:setupCallbackId];
                 [self playerLoop];
             } else {
@@ -64,7 +64,7 @@ BOOL paused = NO;
     } else {
         MusicPlayerStop(mp);
         [self setTime: 0];
-        
+
         stopped = YES;
         paused = NO;
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
@@ -92,14 +92,16 @@ BOOL paused = NO;
     if (ms == nil || released) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     } else {
-        Float64 time = [self getTime];
-        // Don't return more than track length
-        if (time > longestTrackLength) {
-            time = longestTrackLength;
-        }
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%f",time]];
+        MusicTimeStamp position = 0;
+        MusicPlayerGetTime (mp, &position);
+        // Float64 time = [self getTime];
+        // // Don't return more than track length
+        // if (time > longestTrackLength) {
+        //     time = longestTrackLength;
+        // }
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%f",position]];
     }
-    
+
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
@@ -120,13 +122,13 @@ BOOL paused = NO;
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         return;
     }
-    
+
     NSString* timeStr = [command.arguments objectAtIndex:0];
     Float64 time = [timeStr floatValue];
     time = time / 1000;
     //NSLog(@"SetTime: %f", time);
     [self setTime:time];
-    
+
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
@@ -158,46 +160,46 @@ BOOL paused = NO;
 - (BOOL)setupPlayer:(NSString*)path withInstruments:(NSArray*)programs {
     // Initialise the music sequence
     NewMusicSequence(&ms);
-    
+
     NSURL * midiFileURL;
     // NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask,YES);
     // NSString *documentsDirectory = [pathArray objectAtIndex:0];
     // NSString *midiPath = [documentsDirectory stringByAppendingPathComponent:path];
-    
+
     if ([[NSFileManager defaultManager] fileExistsAtPath:path])
     {
         midiFileURL = [NSURL fileURLWithPath:path isDirectory:NO];
     } else {
         return NO;
     }
-    
+
     MusicSequenceFileLoad(ms, (__bridge CFURLRef)(midiFileURL), 0, kMusicSequenceLoadSMF_ChannelsToTracks);
     //MusicSequenceFileLoad(ms, (__bridge CFURLRef)(midiFileURL), 0, 0);
-    
+
     [self setupGraph:programs];
     [self assignInstrumentsToTracks:programs];
-    
+
     NewMusicPlayer(&mp);
-    
+
     // Load the sequence into the music player
     MusicPlayerSetSequence(mp, ms);
-    
+
     MusicPlayerPreroll(mp);
-    
+
     [self setLongestTrackLength];
-    
+
     return YES;
 }
 
 - (void)setLongestTrackLength {
     UInt32 trackCount;
     MusicSequenceGetTrackCount(ms, &trackCount);
-    
+
     MusicTimeStamp longest = 0;
     for (int i = 0; i < trackCount; i++) {
         MusicTrack t;
         MusicSequenceGetIndTrack(ms, i, &t);
-        
+
         MusicTimeStamp len;
         UInt32 sz = sizeof(MusicTimeStamp);
         MusicTrackGetProperty(t, kSequenceTrackProperty_TrackLength, &len, &sz);
@@ -273,43 +275,43 @@ BOOL paused = NO;
     AUNode ioNode, mixerNode;
     AudioUnit samplerUnits[[programs count]];
     AudioUnit ioUnit, mixerUnit;
-    
+
     AudioComponentDescription cd = {};
     cd.componentManufacturer     = kAudioUnitManufacturer_Apple;
-    
+
     //----------------------------------------
     // Add 3 Sampler unit nodes to the graph
     //----------------------------------------
     cd.componentType = kAudioUnitType_MusicDevice;
     cd.componentSubType = kAudioUnitSubType_Sampler;
-    
+
     for (NSInteger i = 0; i < [programs count]; i++) {
         AUNode node;
         AUGraphAddNode (self.processingGraph, &cd, &node);
         samplerNodes[i] = node;
     }
-    
+
     //-----------------------------------
     // 2. Add a Mixer unit node to the graph
     //-----------------------------------
     cd.componentType          = kAudioUnitType_Mixer;
     cd.componentSubType       = kAudioUnitSubType_MultiChannelMixer;
-    
+
     AUGraphAddNode (self.processingGraph, &cd, &mixerNode);
-    
+
     //--------------------------------------
     // 3. Add the Output unit node to the graph
     //--------------------------------------
     cd.componentType = kAudioUnitType_Output;
     cd.componentSubType = kAudioUnitSubType_RemoteIO;  // Output to speakers
-    
+
     AUGraphAddNode (self.processingGraph, &cd, &ioNode);
-    
+
     //---------------
     // Open the graph
     //---------------
     AUGraphOpen (self.processingGraph);
-    
+
     //-----------------------------------------------------------
     // Obtain the mixer unit instance from its corresponding node
     //-----------------------------------------------------------
@@ -319,7 +321,7 @@ BOOL paused = NO;
                      NULL,
                      &mixerUnit
                      );
-    
+
     //--------------------------------
     // Set the bus count for the mixer
     //--------------------------------
@@ -330,48 +332,48 @@ BOOL paused = NO;
                          0,
                          &numBuses,
                          sizeof(numBuses));
-    
-    
-    
+
+
+
     //------------------
     // Connect the nodes
     //------------------
     for (NSInteger i = 0; i < [programs count]; i++) {
         AUGraphConnectNodeInput (self.processingGraph, samplerNodes[i], 0, mixerNode, i);
     }
-    
+
     // Connect the mixer unit to the output unit
     AUGraphConnectNodeInput (self.processingGraph, mixerNode, 0, ioNode, 0);
-    
+
     // Obtain references to all of the audio units from their nodes
     for (NSInteger i = 0; i < [programs count]; i++) {
         AUGraphNodeInfo (self.processingGraph, samplerNodes[i], 0, &samplerUnits[i]);
     }
-    
+
     AUGraphNodeInfo (self.processingGraph, ioNode, 0, &ioUnit);
-    
+
     MusicSequenceSetAUGraph(ms, self.processingGraph);
-    
+
     // Set the instruments
     NSURL * bankURL;
-    
+
     NSString *bankPath = [[NSBundle mainBundle] pathForResource:@"sounds" ofType:@"sf2"];
-    
+
     if ([[NSFileManager defaultManager] fileExistsAtPath:bankPath])
     {
         bankURL = [NSURL fileURLWithPath:bankPath isDirectory:NO];
     } else {
         return;
     }
-    
-    
+
+
     for (NSInteger i = 0; i < [programs count]; i++) {
         AUSamplerBankPresetData bpdata;
         bpdata.bankURL  = (__bridge CFURLRef) bankURL;
         bpdata.bankMSB  = kAUSampler_DefaultMelodicBankMSB;
         bpdata.bankLSB  = kAUSampler_DefaultBankLSB;
         bpdata.presetID = (UInt8) [programs[i] intValue];
-        
+
         AudioUnitSetProperty(samplerUnits[i],
                              kAUSamplerProperty_LoadPresetFromBank,
                              kAudioUnitScope_Global,
@@ -386,20 +388,20 @@ BOOL paused = NO;
     // Set the AUSampler nodes to be used by each track
     //-------------------------------------------------
     MusicTrack tracks[[programs count]];
-    
+
     for (NSInteger i = 0; i < [programs count]; i++) {
         MusicTrack track;
         MusicSequenceGetIndTrack(ms, i, &track);
         tracks[i] = track;
     }
-    
+
     AUNode nodes[[programs count]];
     for (NSInteger i = 0; i < [programs count]; i++) {
         AUNode node;
         AUGraphGetIndNode (self.processingGraph, i, &node);
         nodes[i] = node;
     }
-    
+
     for (NSInteger i = 0; i < [programs count]; i++) {
         MusicTrackSetDestNode(tracks[i], nodes[i]);
     }
